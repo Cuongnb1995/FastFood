9dd3451dd5c9dda0ad41d54320fe60ad7c37f065
CuongNB <cuongdongphu@gmail.com>
2022-06-22 11:00:44 +0700

    Update Search Product

---
+++ Controllers/SearchProductsController.cs
@@ -0,0 +1,20 @@
+﻿using FastFood.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastFood.Controllers
+{
+    public class SearchProductsController : Controller
+    {
+        public FoodDbContext db = new FoodDbContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Search()
+        {
+            string key = !string.IsNullOrEmpty(Request.Query["key"]) ? Request.Query["key"] : "";
+            List<ItemProducts> _ListRecord = db.Products.Where(item => item.Name.Contains(key)).OrderByDescending(item => item.Id).Take(5).ToList();
+            return new JsonResult(Ok(_ListRecord));
+        }
+    }
+}
